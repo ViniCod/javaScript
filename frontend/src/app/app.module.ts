4b805521ee74f07fb6090caef2a5a6fc2b619cff
Vinicius <viniciusprates.6798@aluno.saojudas.br>
2022-04-04 21:42:58 -0300

    Barra de menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IndexComponent } from './home/index/index.component';
 import { HeaderComponent } from './head/header/header.component';
 import { FooterComponent } from './foot/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
